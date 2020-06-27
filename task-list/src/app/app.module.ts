e6f0ea043d06251d1c2f1296573a023bb50245f1
Vaibhav Awale <vaibhavawale3@gmail.com>
2020-06-27 23:56:52 +0530

    initial version

---
+++ task-list/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TaskInputComponent } from './task-input/task-input.component';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      TaskInputComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
